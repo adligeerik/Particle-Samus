6a5a640e782c8a69bbb2c1d28368d72ee68d5585
Erik <erik.von.ke@gmail.com>
2018-12-28 11:42:41 +0100

    almoast done

---
+++ cutSamus.m
@@ -0,0 +1,47 @@
+function [pixelsInSamus] = cutSamus(vidFrame)
+
+rgb = vidFrame(241:288,220:255,:);
+
+k = 8;
+[L,C] = imsegkmeans(rgb,k);
+J = label2rgb(L,im2double(C));
+
+[h,w,color] = size(J);
+J2 = reshape(J,[h*w,color]);
+mask = zeros(h*w,1);
+
+C2 = [C(1,:); C(3,:); C(4,:); C(5,:); C(6,:); C(8,:)];
+
+for ii= 1:length(C2)
+    for jj = 1:h*w
+        if J2(jj,:) == C2(ii,:)
+            mask(jj) =  1;        
+        end
+    end
+
+end
+masksamus = reshape(mask,[h,w,1]);
+
+extrapixles = [16 7; 17 7; 16 8; 17 8; 16 9; 17 9; 15 10; 20 10; 18 12; 25 16; 22 31; 22 32; 20 33; 21 33; 22 33; 20 34; 21 34; 22 34];
+masksamus(extrapixles(:,2), extrapixles(:,1)) = 1;
+
+masksamus = reshape(masksamus,[h*w,1]);
+
+nrgb = reshape(rgb,[h*w,color]);
+pixelsInSamus = ones(size(nrgb));
+% kk = 1;
+% for ii = 1: length(pixelsInSamus)
+%     if masksamus(ii) == 1
+%         pixelsInSamus(ii,:) = nrgb(ii,:);
+%         kk = kk + 1;
+%     end
+%     
+% end
+
+pixelsInSamus = nrgb(logical(masksamus),:);
+
+masksamus = reshape(pixelsInSamus,[h,w, 3]);
+imshow(uint8(masksamus))
+
+end
+
