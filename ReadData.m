2772c982198fb3734c817f0037064ae41f409a24
Erik <erik.von.ke@gmail.com>
2018-12-28 10:22:11 +0100

    more lines

--- ReadData.m
+++ ReadData.m
@@ -10,7 +10,35 @@ currAxes = axes;
 %end
 
 
-%Samus
+%Samus 
 rgb = vidFrame(241:288,220:255,:);
 
+
+%% Kmeans samus
+k = 8;
+[L,C] = imsegkmeans(rgb,k);
+J = label2rgb(L,im2double(C));
+subplot(1,2,1)
 imshow(rgb)
+subplot(1,2,2)
+imshow(J)
+
+
+%% Histogram
+
+[h,w,color] = size(J)
+
+J2 = reshape(J,[h*w,color]);
+
+for ii= 1:k
+    kk = 0;
+    for jj = 1:h*w
+        if J2(jj,1) == C(ii,1)
+            kk = kk+1;
+        
+        end
+    end
+Tot(ii) = kk;
+end
+subplot(1,2,1)
+bar(Tot)
