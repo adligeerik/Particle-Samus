3afc8f79d6f633f8160ef5f9b5253d53bf2bbdf6
svengre <sven.grenholm@gmail.com>
2018-12-29 14:18:01 +0100

    Sample_histogram in progress

--- Particle_filter.m
+++ Particle_filter.m
@@ -1,9 +1,10 @@
 function Xbar = Particle_filter(X,pzx,height,width,binx,biny,currentframe)
 M = length(x)
+Xbar = zeros(size(X));
 
 X = Control_input(X);
 histogram = makehistogram(X,height,width,binx,biny);
-Xbar = Sample_histogram(histogram );
+Xbar(:,1:2) = Sample_histogram(histogram,height,width,binx,biny,M );
 Xbar(:,3) = assignweight(Xbar(:,1:2),pzx,currentframe);
 Xbar   = Low_variance_sampler(Xbar);
 
---
+++ Sample_histogram.m
@@ -0,0 +1,15 @@
+function Xpos = Sample_histogram(histogram,height,width,binx,biny,M )
+
+probabilitydensity = zeros(width,height)
+
+for ii = 1:binx
+    for jj = 1:biny
+
+
+for m = 1:M
+    r =  
+    
+    
+    
+end
+end
