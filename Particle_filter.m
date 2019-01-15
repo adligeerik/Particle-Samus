5774d77b5f3fd3c486bd979e6a88f814543b79cb
svengre <sven.grenholm@gmail.com>
2019-01-15 20:04:27 +0100

    sample normal modified, particle_filter modified

--- Particle_filter.m
+++ Particle_filter.m
@@ -1,4 +1,4 @@
-function Xbar = Particle_filter(X,pzx,height,width,binx,biny,currentframe)
+function Xbar,mu = Particle_filter(X,pzx,height,width,currentframe)
 
 
 Xbar = zeros(size(X));
@@ -6,9 +6,14 @@ Xbar = zeros(size(X));
 M = length(X);
 
 
+% X = Control_input(X,height,width);
+% histogram = makehistogram(X,height,width,binx,biny);
+% Xbar(:,1:2) = Sample_histogram(histogram,height,width,binx,biny,M );
+% Xbar(:,3) = assignweight(Xbar(:,1:2),pzx,currentframe);
+% Xbar   = Low_variance_sampler(Xbar);
+
 X = Control_input(X,height,width);
-histogram = makehistogram(X,height,width,binx,biny);
-Xbar(:,1:2) = Sample_histogram(histogram,height,width,binx,biny,M );
+[X(:,1:2),mu,Sigma] = sample_normal(X,width,height)
 Xbar(:,3) = assignweight(Xbar(:,1:2),pzx,currentframe);
 Xbar   = Low_variance_sampler(Xbar);
 
--- sample_normal.m
+++ sample_normal.m
@@ -1,8 +1,19 @@
 function [Xbar,mu,Sigma] = sample_normal(X,width,height)
 M = length(X)
-weightedpos = (X(:,1:2).*X(:,3));
-mu          = sum(weightedpos);
-Sigma       = cov(M*weightedpos);
-Xpos        =  mvnrnd(MU,SIGMA,M);
+%weightedpos = (X(:,1:2).*X(:,3));
+mu          = sum(X(:,1:2))/M;
+Sigma       = cov(X(:,1:2));
+Xbar        =  round(mvnrnd(mu,Sigma,M));
+x = Xbar(:,1);
+y = Xbar(:,2);
+
+x(x<1) = 1;
+y(y<1) = 1;
+
+x(x>width) = width;
+y(y>height) = height;
+
+Xbar(:,1) = x;
+Xbar(:,2) = y;
 
 end
