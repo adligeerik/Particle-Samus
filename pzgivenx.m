c371ef6da37c0e8de1fe0777aa8a9bf965b57245
svengre <sven.grenholm@gmail.com>
2019-01-20 16:32:43 +0100

    histogram pzx first semi-working prototype

--- pzgivenx.m
+++ pzgivenx.m
@@ -1,16 +1,19 @@
 function p = pzgivenx(samus)
 
 samus = double(samus);
-
-p = zeros(256,256,256);
+b = 5;
+p = zeros(256+2*b,256+2*b,256+2*b);
 
 N = length(samus);
 
 samus = samus + [1 1 1];
-for ii = 1:N
-    p(samus(ii,1),samus(ii,2),samus(ii,3)) = p(samus(ii,1),samus(ii,2),samus(ii,3)) + 1;
-end
 
+adding = ones(2*b+1,2*b+1,2*b+1); 
+adding(b+1,b+1,b+1) = 2;
+for ii = 1:N
+    p(samus(ii,1):samus(ii,1)+2*b,samus(ii,2):samus(ii,2)+2*b,samus(ii,3):samus(ii,3)+2*b) = p(samus(ii,1):samus(ii,1)+2*b,samus(ii,2):samus(ii,2)+2*b,samus(ii,3):samus(ii,3)+2*b) + adding;
+end
+p = p(b+1:256+b,b+1:256+b,b+1:256+b);
 
 p(1:3,1:3,1:3) = 0;
 end 
