7ceb93970ee47afeb6bcf45d5f6dc6f4b798fe76
svengre <sven.grenholm@gmail.com>
2019-02-21 18:56:10 +0100

    Main_Samus2

--- Sample_histogram.m
+++ Sample_histogram.m
@@ -3,7 +3,7 @@ binpix = width/binx;
 binpiy = height/biny;
 Xpos   = zeros(M,2);
 
-probabilitydensity = zeros(width,height)
+probabilitydensity = zeros(binx,biny);
 w = 0;
 for ii = 1:binx
     for jj = 1:biny
@@ -17,13 +17,13 @@ for m = 1:M
     for ii = 1:binx
         for jj = 1:biny
             c = probabilitydensity(ii,jj);
-            if r =< c
+            if r <= c
                 Xpos(m,1) = (ii-1)*binpix + 1 + int8(rand*(binpix-1));
                 Xpos(m,2) = (jj-1)*binpiy + 1 + int8(rand*(binpiy-1));
                 break
             end       
         end
-        if r =< c
+        if r <= c
             break
         end
     end   
--- makehistogram.m
+++ makehistogram.m
@@ -1,9 +1,9 @@
 function histogram = makehistogram(particles,height,width,binx,biny)
-
+histogram = zeros(binx,biny);
 binx = width/binx;
 biny = height/biny;
 
-histogram = zeros(binx,biny);
+
 
 particles(:,1) = ceil(particles(:,1)/binx);
 particles(:,2) = ceil(particles(:,2)/biny );
@@ -11,8 +11,8 @@ particles(:,2) = ceil(particles(:,2)/biny );
 for ii = 1:length(particles)
     x = particles(ii,1);
     y = particles(ii,2);
-    w = particles(ii,3);
-    histogram(x,y) = histogram(x,y) + w;
+    %w = particles(ii,3);
+    histogram(x,y) = histogram(x,y) + 1;
     
 end 
 
